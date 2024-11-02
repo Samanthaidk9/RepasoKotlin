dba25bc7262ed8dbf59d205ac88ae4255e42b187
Samantha <ksva745@gmail.com>
2024-11-02 09:11:55 -0500

    Primer cargue

---
+++ src/Main.kt
@@ -0,0 +1,40 @@
+fun main() {
+   /* for (i in 1..10) {
+        println(i)
+    }
+
+    for (j in 1..10 step 3) {
+        println(j)
+    }
+
+    for(x in 100 downTo 1 step 3) {
+        println(x)
+    }
+    */
+    /*
+    var tabla = 3
+
+    for (i in 1..10) {
+        println("${tabla} * ${i} = ${tabla*i}")
+    }
+    */
+
+    var z = 1
+
+    while(z<=10){
+
+        println(z)
+        z++
+    }
+
+    //do while
+
+    var m = 100
+
+    do{
+        println(m)
+        m--
+    }while(m>=1)
+
+
+}
